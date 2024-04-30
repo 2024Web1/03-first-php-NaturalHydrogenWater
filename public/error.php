824dd50ab1d7a675122eba4d7be1ea875d26fcb1
NaturalHydrogenWater <kd1341034@st.kobedenshi.ac.jp>
2024-04-30 13:54:31 +0900

    エラーの修正

--- public/error.php
+++ public/error.php
@@ -5,12 +5,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>エラーPHP</title>
+    <title>エラーPHP</title> <!-- タイトルも変更する -->
 </head>
 
 <body>
     <?php
-        echo <p>Hello,PHP</p>;
+    echo Hello,PHP; // 文字列をシングルクォーテーションで囲まなかった  
     ?>
     <p>0J0X0XX神戸電子</p>
 </body>
--- public/hello.php
+++ public/hello.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="ja">
+<!DOCTYPE html> <!-- ① -->
+<!-- 今回は初回なので、仮のファイルを用意しましたが、今後は指示に従い、自分でファイルを作っていって下さい -->
+<html lang="ja"> <!-- ② -->
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> <!-- ③ -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php echo("Hello,PHP"); ?>
-    <p>0J0400X神戸電子</p>
+    <!-- この領域(bodyタグ内)に処理内容を入れてください -->
+      <?php // ④
+        echo '<p>Hello,PHP</p>'; // ⑤
+        ?> <!-- ⑥ -->
+    <p>0J0X00X神戸電子</p>
+    <!-- 今回はここまで -->
 </body>
 </html>
